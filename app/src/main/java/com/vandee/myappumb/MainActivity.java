e398396e27b3d7d54521be3a48ac60eb5a80142e
fandi21 <fandi.smart21@gmail.com>
2024-11-13 19:15:42 +0700

    commit

--- app/src/main/java/com/vandee/myappumb/MainActivity.java
+++ app/src/main/java/com/vandee/myappumb/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import com.vandee.myappumb.intro.*;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
--- app/src/main/java/com/vandee/myappumb/intro/IntroActivity.java
+++ app/src/main/java/com/vandee/myappumb/intro/IntroActivity.java
@@ -1,6 +1,8 @@
 package com.vandee.myappumb.intro;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager.widget.ViewPager;
+import com.vandee.myappumb.adapter.MpagerAdapter;
 
 import com.vandee.myappumb.R;
 
@@ -15,6 +18,10 @@ public class IntroActivity extends AppCompatActivity {
 
     private ViewPager mPager;
     private int[] layouts = {R.layout.first_slide, R.layout.second_slide, R.layout.third_slide};
+    private MpagerAdapter mpagerAdapter;
+
+    private LinearLayout Dots_layout;
+    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,8 @@ public class IntroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intro);
 
         mPager = (ViewPager) findViewById(R.id.viewPager);
+        mpagerAdapter = new MpagerAdapter(layouts, this);
+        mPager.setAdapter(mpagerAdapter);
 
     }
 }
