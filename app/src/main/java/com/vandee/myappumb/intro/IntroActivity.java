d47cc4cd7298cefff57c77d377eebb219952baae
fandi21 <fandi.smart21@gmail.com>
2024-11-13 19:49:04 +0700

    commit

--- app/src/main/java/com/vandee/myappumb/intro/IntroActivity.java
+++ app/src/main/java/com/vandee/myappumb/intro/IntroActivity.java
@@ -1,11 +1,13 @@
 package com.vandee.myappumb.intro;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,10 +18,12 @@ import com.vandee.myappumb.R;
 
 public class IntroActivity extends AppCompatActivity {
 
+    //Set variable pager
     private ViewPager mPager;
     private int[] layouts = {R.layout.first_slide, R.layout.second_slide, R.layout.third_slide};
     private MpagerAdapter mpagerAdapter;
 
+    //Set variable dots
     private LinearLayout Dots_layout;
     private ImageView[] dots;
 
@@ -33,5 +37,29 @@ public class IntroActivity extends AppCompatActivity {
         mpagerAdapter = new MpagerAdapter(layouts, this);
         mPager.setAdapter(mpagerAdapter);
 
+        Dots_layout = (LinearLayout) findViewById(R.id.dotslayout);
+
+        createDots(0);
+    }
+
+    private void createDots(int current_position){
+        if(Dots_layout != null)
+            Dots_layout.removeAllViews();
+
+        dots = new ImageView[layouts.length];
+        for(int i = 0; i < layouts.length; i++){
+            dots[i] = new ImageView(this);
+            if(i == current_position){
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.active_dots));
+            }else {
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.default_dots));
+            }
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(4, 0, 4, 0);
+
+            Dots_layout.addView(dots[i], params);
+        }
     }
 }
